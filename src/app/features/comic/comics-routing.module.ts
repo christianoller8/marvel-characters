b8f4d0e27b2a5d5e129c0fb9b96a828748f97193
Christian Oller <christianoller.dev@gmail.com>
2023-04-24 12:35:01 +0200

    Comics updated

---
+++ src/app/features/comic/comics-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CarouselComponent } from "./components/carousel/carousel.component";
+import { ComicPageComponent } from "./components/comic-page/comic-page.component";
+import { ComicListComponent } from "./components/comic-list/comic-list.component";
+import { ComicComponent } from "./components/comic/comic.component";
+import { ComicDetailComponent } from "./components/comic-detail/comic-detail.component";
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HeroesRoutingModule {}
