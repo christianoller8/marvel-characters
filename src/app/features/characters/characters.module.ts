9c6eb34c370a1eaa1d777ecfce567d8e61d2e0c9
Christian Oller <christianoller.dev@gmail.com>
2023-04-28 09:11:34 +0200

    Carousel and comic style added

--- src/app/features/characters/characters.module.ts
+++ src/app/features/characters/characters.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { CharacterPageComponent } from "./components/character-page/character-page.component";
 import { CharacterSelectorComponent } from "./components/character-selector/character-selector.component";
 import { NgxPaginationModule } from "ngx-pagination";
+import { HeroesRoutingModule } from "./characters-routing.module";
 
 @NgModule({
   declarations: [CharacterPageComponent, CharacterSelectorComponent],
-  imports: [CommonModule, NgxPaginationModule],
+  imports: [CommonModule, HeroesRoutingModule, NgxPaginationModule],
   exports: [CharacterPageComponent, CharacterSelectorComponent],
 })
 export class CharactersModule {}
--- src/app/shared/components/material/material.module.ts
+++ src/app/shared/components/material/material.module.ts
@@ -1,13 +1,38 @@
 import { NgModule } from "@angular/core";
 
-import { MatCardModule } from "@angular/material/card";
-import { MatInputModule } from "@angular/material/input";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatButtonModule } from "@angular/material/button";
-
-const modules = [MatCardModule, MatInputModule, MatButtonModule];
+import { MatCardModule } from "@angular/material/card";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
-  imports: modules,
-  exports: modules,
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
